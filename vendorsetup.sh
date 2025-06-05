42b952a2697a8a0a075735690f8816fb28a0126c
neophyte-w <122590776+neophyte-w@users.noreply.github.com>
2025-06-05 12:14:32 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
+add_lunch_combo omni_RMX2202
 add_lunch_combo omni_lahaina-user
 add_lunch_combo omni_lahaina-userdebug
 add_lunch_combo omni_lahaina-eng
